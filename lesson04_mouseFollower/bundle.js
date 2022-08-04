7e4b4c054bed6ab3c1776afe049941a592605695
tade <tadeojcocucci@gmail.com>
2022-08-04 19:44:14 -0300

    rehago lesson04

--- lesson04_mouseFollower/bundle.js
+++ lesson04_mouseFollower/bundle.js
@@ -6,28 +6,27 @@
   var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
   var ReactDOM__default = /*#__PURE__*/_interopDefaultLegacy(ReactDOM);
 
-  var width = 960;
-  var height = 500;
   var radius = 10;
+  var width = 200;
+  var height = 200;
   var initialMousePosition = {x: width / 2, y: height / 2};
-
 
   var App = function () {
     var ref = React.useState(initialMousePosition);
     var mousePosition = ref[0];
     var setMousePosition = ref[1];
-    var handleMouseMove = React.useCallback(function (event) {
-      var clientX = event.clientX;
-      var clientY = event.clientY;
+    var handleMouseMove = React.useCallback(function (ref) {
+      var clientX = ref.clientX;
+      var clientY = ref.clientY;
+
       setMousePosition({x: clientX, y: clientY});
     }, [setMousePosition]);
+    console.log(mousePosition.x, mousePosition.y);
     return (
       React__default['default'].createElement( 'svg', { width: width, height: height, onMouseMove: handleMouseMove },
-        React__default['default'].createElement( 'circle', {
-          r: radius, cx: mousePosition.x, cy: mousePosition.y })
+        React__default['default'].createElement( 'circle', { cx: mousePosition.x, cy: mousePosition.y, r: radius })
       )
-    );
-  };
+  )};
 
   var rootElement = document.getElementById("root");
   ReactDOM__default['default'].render(React__default['default'].createElement( App, null ), rootElement);
--- lesson04_mouseFollower/index.js
+++ lesson04_mouseFollower/index.js
@@ -1,28 +1,22 @@
 import React, { useState, useCallback } from 'react';
 import ReactDOM from 'react-dom';
 
-const width = 960;
-const height = 500;
 const radius = 10;
+const width = 200;
+const height = 200;
 const initialMousePosition = {x: width / 2, y: height / 2};
-
 
 const App = () => {
   const [mousePosition, setMousePosition] = useState(initialMousePosition);
-  const handleMouseMove = useCallback((event) => {
-    const { clientX, clientY } = event;
+  const handleMouseMove = useCallback(({ clientX, clientY }) => {
     setMousePosition({x: clientX, y: clientY});
   }, [setMousePosition]);
+  console.log(mousePosition.x, mousePosition.y);
   return (
     <svg width={width} height={height} onMouseMove={handleMouseMove}>
-      <circle
-        r={radius}
-        cx={mousePosition.x}
-        cy={mousePosition.y}
-      />
+      <circle cx={mousePosition.x} cy={mousePosition.y} r={radius}></circle>
     </svg>
-  );
-};
+)};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
